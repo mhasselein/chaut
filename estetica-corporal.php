ba17ab36f31e1578984e61205fbcad07e3edb7c4
Mateus Hasselein <mateushass82@gmail.com>
2017-03-29 17:20:33 -0300

    estetica-corporal.php

---
+++ estetica-corporal.php
@@ -0,0 +1,246 @@
+ <!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <!-- Basic Page Needs ================================================== -->
+        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel="icon" type="image/png" href="images/favicon.png">
+        <title>Estética Corporal - Clínica de Estética - Clínica Haut</title>
+        <meta name="description" content="Clínica Haut oferece o que existe de melhor em estética corporal da atualidade. Lipocavitação, Radiofrequência, Drenagem linfática, Carboxiterapia.">
+        <meta name="keywords" content="clinica, Estética Corporal, Clínica de Estética, Clínica Haut, Lipocavitação, Gordura, Mesoterapia, Estética, Carboxiterapia, Radiofrequência, tratamento estético, Endermologia, celulite, Drenagem Linfática ">
+        <meta name="author" content="Clinica Haut">
+        <meta name="robots" content="index, follow">
+        <!-- Mobile Specific Metas ================================================== -->
+        <meta name="format-detection" content="telephone=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Template CSS Files ================================================== -->
+        <!-- Twitter Bootstrs CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Ionicons Fonts Css -->
+        <link rel="stylesheet" href="css/ionicons.min.css">
+        <!-- animate css -->
+        <link rel="stylesheet" href="css/animate.css">
+        <!-- Hero area slider css-->
+        <link rel="stylesheet" href="css/slider.css">
+        <!-- owl craousel css -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        <link rel="stylesheet" href="css/owl.theme.css">
+        <link rel="stylesheet" href="css/jquery.fancybox.css">
+        <!-- template main css file -->
+        <link rel="stylesheet" href="css/main.css">
+        <!-- responsive css -->
+        <link rel="stylesheet" href="css/responsive.css">
+
+        <!-- Template Javascript Files================================================== -->
+        <!-- modernizr js -->
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <!-- jquery -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <!-- owl carouserl js -->
+        <script src="js/owl.carousel.min.js"></script>
+        <!-- bootstrap js -->
+
+        <script src="js/bootstrap.min.js"></script>
+        <!-- wow js -->
+        <script src="js/wow.min.js"></script>
+        <!-- slider js -->
+        <script src="js/slider.js"></script>
+        <script src="js/jquery.fancybox.js"></script>
+        <!-- template main js -->
+        <script src="js/main.js"></script>
+    </head>
+    <body>
+        <?php include_once("./php/analyticstracking.php") ?>
+        <!--==================================================
+        Header 
+        ================================================== -->
+        <?php include_once 'header.php'; ?>
+        
+        <!--================================================== 
+            Global Page Section Start
+        ================================================== -->
+        <section class="global-page-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="block">
+                            <h2>Estética Corporal</h2>
+                            <ol class="breadcrumb">
+                                <li>
+                                    <a href="index.php">
+                                        <i class="ion-ios-home"></i>
+                                        Home
+                                    </a>
+                                </li>
+                                <li class="active">Estética Corporal</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>   
+        </section>
+        <!-- ================================================== 
+            Works Section Start
+        ================================================== -->
+        <section class="works service-page">
+            <div class="container">
+                <h2 class="subtitle wow fadeInUp animated" data-wow-delay=".3s" data-wow-duration="500ms">Tratamentos Estéticos Corporais</h2>
+                <p class="subtitle-des wow fadeInUp animated" data-wow-delay=".5s" data-wow-duration="500ms">
+                    A <strong>Clínica Haut</strong> é uma <strong>clínica de estética</strong> que oferece um ambiente para que você sinta-se à vontade para relaxar e cuidar de sua beleza, <br>deixe-se envolver pela suavidade de nosso ambiente totalmente preparado para o seu conforto.
+                </p>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <figure class="wow fadeInLeft animated portfolio-item" data-wow-duration="500ms" data-wow-delay="0ms">
+                            <div class="img-wrapper">
+                                <img src="images/portfolio/corporal-lipocavitacao.jpg" class="img-responsive" alt="Lipocavitação" >
+                                <div class="overlay">
+                                    <div class="buttons">
+                                        <a rel="gallery" class="fancybox" href="images/portfolio/corporal-lipocavitacao.jpg">Slide</a>        
+                                        <a  href="corporal-lipocavitacao.php">Detalhes</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <figcaption>
+                                <h4>
+                                    <a href="corporal-lipocavitacao.php">
+                                        Lipocavitação        
+                                    </a>
+                                </h4>
+                                <p >
+                                    A <strong>lipocavitação</strong> é um <strong>procedimento estético</strong> realizado através de um aparelho de ultrassom focado que serve para eliminar <strong>gordura acumulada na barriga, coxas, flancos</strong>...
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+
+                    <div class="col-sm-3">
+                        <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="300ms">
+                            <div class="img-wrapper">
+                                <img src="images/portfolio/corporal-mesoterapia.jpg" class="img-responsive" alt="Mesoterapia" >
+                                <div class="overlay">
+                                    <div class="buttons">
+                                        <a rel="gallery" class="fancybox" href="images/portfolio/corporal-mesoterapia.jpg">Slide</a>        
+                                        <a  href="corporal-mesoterapia.php">Detalhes</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <figcaption>
+                                <h4>
+                                    <a href="corporal-mesoterapia.php">
+                                        Mesoterapia        
+                                    </a>
+                                </h4>
+                                <p>
+                                    A <strong>Mesoterapia</strong> (aplicação de enzimas e outras medicações), chamada também de <strong>Intradermoterapia</strong> é uma técnica de origem francesa amplamente utilizada na <strong>estética</strong> por se ...
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+
+                    <div class="col-sm-3">
+                        <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="300ms">
+                            <div class="img-wrapper">
+                                <img src="images/portfolio/corporal-carboxiterapia.jpg" class="img-responsive" alt="Carboxiterapia" >
+                                <div class="overlay">
+                                    <div class="buttons">
+                                        <a rel="gallery" class="fancybox" href="images/portfolio/corporal-carboxiterapia.jpg">Slide</a>        
+                                        <a  href="corporal-carboxiterapia.php">Detalhes</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <figcaption>
+                                <h4>
+                                    <a href="corporal-carboxiterapia.php">
+                                        Carboxiterapia        
+                                    </a>
+                                </h4>
+                                <p>
+                                    A <strong>carboxiterapia</strong> é um <strong>tratamento estético</strong> realizado através da infusão de gás carbônico medicinal em diferentes camadas da <strong>pele</strong>, promovendo uma melhor oxigenação, aumento da ...
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+
+                    <div class="col-sm-3">
+                        <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="600ms">
+                            <div class="img-wrapper">
+                                <img src="images/portfolio/corporal-radiofrequencia.jpg" class="img-responsive" alt="Radiofrequência" >
+                                <div class="overlay">
+                                    <div class="buttons">
+                                        <a rel="gallery" class="fancybox" href="images/portfolio/corporal-radiofrequencia.jpg">Slide</a>        
+                                        <a  href="corporal-radiofrequencia.php">Detalhes</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <figcaption>
+                                <h4>
+                                    <a href="corporal-radiofrequencia.php">
+                                        Radiofrequência        
+                                    </a>
+                                </h4>
+                                <p>
+                                    <strong>Radiofrequência</strong> tem como objetivo aumentar a temperatura do tecido, a fim de alcançar cerca de 40°C, desencadeando uma sequência de reações fisiológicas. O aquecimento do tecido ...
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="col-sm-3">
+                        <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="600ms">
+                            <div class="img-wrapper">
+                                <img src="images/portfolio/corporal-endermologia.jpg" class="img-responsive" alt="Endermologia" >
+                                <div class="overlay">
+                                    <div class="buttons">
+                                        <a rel="gallery" class="fancybox" href="images/portfolio/corporal-endermologia.jpg">Slide</a>        
+                                        <a  href="corporal-endermologia.php">Detalhes</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <figcaption>
+                                <h4>
+                                    <a href="corporal-endermologia.php">
+                                        Endermologia        
+                                    </a>
+                                </h4>
+                                <p>
+                                    A <strong>Endermologia</strong> é um procedimento que associa massagem à vacuoterapia, aumenta o fluxo sanguíneo, promove o aumento da nutrição das células e desmancha os nódulos da <strong>celulite</strong>. O aparelho ...
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="col-sm-3">
+                        <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="600ms">
+                            <div class="img-wrapper">
+                                <img src="images/portfolio/corporal-drenagem-linfatica-manual.jpg" class="img-responsive" alt="Drenagem Linfática Manual " >
+                                <div class="overlay">
+                                    <div class="buttons">
+                                        <a rel="gallery" class="fancybox" href="images/portfolio/corporal-drenagem-linfatica-manual.jpg">Slide</a>        
+                                        <a  href="corporal-drenagem-linfatica-manual.php">Detalhes</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <figcaption>
+                                <h4>
+                                    <a href="corporal-drenagem-linfatica-manual.php">
+                                        Drenagem Linfática Manual        
+                                    </a>
+                                </h4>
+                                <p>
+                                    A <strong>Drenagem Linfática</strong> é uma técnica de massagem muito suave que tem por objetivo estimular o sistema linfático a trabalhar de forma mais acelerada. Entre os principais benefícios do método...
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ================================================== 
+           Contato Footer
+        ================================================== -->
+        <?php include_once 'contato_footer.php'; ?>
+        <!-- ================================================== 
+            Footer Section Start
+        ================================================== -->
+        <?php include_once 'footer.php' ?>
+    </body>
+</html>
