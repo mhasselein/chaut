2c08c11366907cbfe9a3417ac1bfd3301690e4df
mateus <mateus82@gmail.com>
2017-11-06 15:01:41 -0200

    removivo alguns links

--- index.php
+++ index.php
@@ -20,9 +20,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- banner -->
         <link rel="stylesheet" href="themes/default/default.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="themes/light/light.css" type="text/css" media="screen" />
+<!--        <link rel="stylesheet" href="themes/light/light.css" type="text/css" media="screen" />
         <link rel="stylesheet" href="themes/dark/dark.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="themes/bar/bar.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="themes/bar/bar.css" type="text/css" media="screen" />-->
         <link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
 
         <!-- Template CSS Files
