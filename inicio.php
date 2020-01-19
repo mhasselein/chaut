17db70f3c0a23ab54c61cd3e164afb2f466d4c38
mhasselein <mateushass82@gmail.com>
2020-01-19 20:30:27 -0300

    inicio

---
+++ inicio.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <!-- Basic Page Needs ================================================== -->
+        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel="icon" type="image/png" href="images/favicon.png">
+        <title>Clínica Haut - Clínica de Estética - Bento Gonçalves RS</title>
+        <meta name="description" content="Clínica Estética localizada em Bento Gonçalves. Tratamentos para celulite, estrias, flacidez, gordura localizada, acne, olheiras e pelos indesejados.">
+        <meta name="keywords" content="Rugas, Clínica Haut, Estética, Biomedicina, Queda de Cabelo, Calvicie, Estética Corporal, Estética Facial, Acne, Celulite, Bento Gonçalves, Depilação, Fotodepilação, Clinica de estetica, PEIM, Pós Operatório">
+        <meta name="author" content="Clinica Haut">
+        <meta name="robots" content="index, follow"> 
+        <meta name="google-site-verification" content="8z7CvL3Rp1I0ADPIDYLO4E12ZwjdbjB4pohwdkDTiZU" />
+        <!-- Mobile Specific Metas ================================================== -->
+        <meta name="format-detection" content="telephone=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- banner -->
+        <link rel="stylesheet" href="themes/default/default.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+        <!-- Template CSS Files ================================================== -->
+        <!-- Twitter Bootstrs CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Ionicons Fonts Css -->
+        <link rel="stylesheet" href="css/ionicons.min.css">
+        <!-- animate css -->
+        <link rel="stylesheet" href="css/animate.css">
+        <!-- Hero area slider css-->
+        <link rel="stylesheet" href="css/slider.css">
+        <!-- owl craousel css -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        <link rel="stylesheet" href="css/owl.theme.css">
+        <link rel="stylesheet" href="css/jquery.fancybox.css">
+        <!-- template main css file -->
+        <link rel="stylesheet" href="css/main.css">
+        <!-- responsive css -->
+        <link rel="stylesheet" href="css/responsive.css">
+        <!-- Template Javascript Files ================================================== -->
+        <!-- modernizr js -->
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <!-- jquery -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <!-- owl carouserl js -->
+        <script src="js/owl.carousel.min.js"></script>
+        <!-- bootstrap js -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- wow js -->
+        <script src="js/wow.min.js"></script>
+        <!-- slider js -->
+        <script src="js/slider.js"></script>
+        <script src="js/jquery.fancybox.js"></script>
+        <!-- template main js -->
+        <script src="js/main.js"></script>
+    </head>
+    <body>
+        <?php include_once("./php/analyticstracking.php") ?>
+       <?php include_once 'header.php';?>
+        <!--================================Slider Section Start================================================== -->
+        <section id="banner" >
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <div class="slider-wrapper theme-default">
+                            <div id="slider" class="nivoSlider">
+                                <img src="images/b3.jpg" data-thumb="images/b3.jpg" alt="Estética Facial"  data-transition="boxRainReverse" />
+                                <img src="images/b1.jpg" data-thumb="images/b1.jpg" alt="Fotodepilação" data-transition="boxRainReverse" />
+                                <img src="images/b2.jpg" data-thumb="images/b2.jpg" alt="Pós Operatório" data-transition="boxRainReverse" />
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+        </section>
+            <!--==================================================Slider Section Start================================================== -->
+            <section id="about" >
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-7 col-sm-7" >
+                            <div class="block wow fadeInLeft" data-wow-delay=".3s" data-wow-duration="500ms">
+                                <h1>Clínica Haut Biomedicina e Estética</h1>
+                                <p>Encontre aqui os mais avançados tratamentos de <em>estética corporal</em>, tratamentos de <em>estética facial</em> e tratamentos de <em>estética capilar</em> utilizando os equipamentos e técnicas de <strong>estética</strong> mais avançados da atualidade..</p>
+                                <p>Temos um cuidado especial com seu <em>bem-estar</em>, pois sabemos que sua <em>beleza</em> é resultado de uma união saudável entre mente e <em>corpo</em>.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-5 col-sm-5" >
+                            <div class="block wow fadeInRight" data-wow-delay=".3s" data-wow-duration="500ms">
+                                <img src="images/about/about.png" alt="Clínica Haut">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!--==================================================Portfolio Section Start================================================== -->
+            <section id="works" class="works">
+                <div class="container">
+                    <div class="section-heading">
+                        <h1 class="title wow fadeInDown" data-wow-delay=".3s">Tratamentos Estéticos</h1>
+                        <p class="wow fadeInDown" data-wow-delay=".5s">
+                            A <strong>Clínica Haut</strong> possui o que existe de melhor em tecnologia e profissionais qualificados. 
+                            Está habilitada a oferecer tratamentos  para <strong>gordura localizada, celulite, olheiras, calvicie, manchas na pele, 
+                            flacidez, estrias, rejuvenescimento facial, acne, depilação, preenchimentos, pós operatórios</strong> e muito mais... 
+                        </p>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-4 col-xs-12">
+                            <figure class="wow fadeInLeft animated portfolio-item" data-wow-duration="500ms" data-wow-delay="3ms">
+                                <div class="img-wrapper">
+                                    <img src="images/portfolio/facial-microagulhamento.jpg" class="img-responsive" alt="Estética Facial" >
+                                    <div class="overlay">
+                                        <div class="buttons" style="left: 30%">
+                                            <a href="estetica-facial.php">Mais Detalhes</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <figcaption>
+                                <h4><a href="estetica-facial.php">Estética Facial</a></h4>
+                                <p>
+                                    <a href="facial-limpeza-pele.php">Limpeza de Pele</strong></a>, 
+                                    <a href="facial-peeling-quimico.php">Peeling Químico</a>, 
+                                    <a href="facial-peeling-diamante.php">Peeling de Diamante</a>, 
+                                    <a href="facial-radiofrequencia.php">Radiofrequência</a>, 
+                                    <a href="facial-botox.php">Botox</a>, 
+                                    <a href="facial-preenchimento-acido-hialuronico.php">Preenchimento com Ácido Hialurônico</a>,
+                                    <a href="facial-carboxiterapia.php"> Carboxiterapia</a> e 
+                                    <a href="facial-microagulhamento.php">Microagulhamento</a>.
+                                </p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div class="col-sm-4 col-xs-12">
+                            <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="300ms">
+                                <div class="img-wrapper">
+                                    <img src="images/portfolio/corporal-lipocavitacao.jpg" class="img-responsive" alt="Estética Corporal" >
+                                    <div class="overlay">
+                                        <div class="buttons" style="left: 30%">
+                                            <a href="estetica-corporal.php">Mais Detalhes</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <figcaption>
+                                <h4>
+                                    <a href="estetica-corporal.php">Estética Corporal</a>
+                                </h4>
+                                <p>
+                                    <a href="corporal-lipocavitacao.php">Lipocavitação</a>, 
+                                    <a href="corporal-carboxiterapia.php">Carboxiterapia</a>, 
+                                    <a href="corporal-drenagem-linfatica_manual.php">Drenagem Linfática Manual</a>, 
+                                    <a href="corporal-endermologia.php">Endermologia</a>, 
+                                    <a href="corporal-mesoterapia.php">Mesoterapia</a>, 
+                                    <a href="corporal-radiofrequencia.php">Radiofrequência</a>, 
+                                    <a href="fotodepilacao.php">Fotodepilação</a>,
+                                    <a href="corporal-peim.php">PEIM.</a><br>&nbsp
+                                </p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div class="col-sm-4 col-xs-12">
+                            <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="300ms">
+                                <div class="img-wrapper">
+                                    <img src="images/portfolio/microagulhamento_capilar.jpg" class="img-responsive" alt="" >
+                                    <div class="overlay">
+                                        <div class="overlay">
+                                        <div class="buttons" style="left: 30%">
+                                            <a  href="estetica-capilar.php">Mais Detalhes</a>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
+                                <figcaption>
+                                <h4>
+                                <a href="estetica-capilar.php">Estética Capilar</a>
+                                </h4>
+                                <p>
+                                    <a href="capilar-microagulhamento.php">Microagulhamento Capilar (Dermaroler)</a>, 
+                                    <a href="capilar-mesoterapia.php">Mesoterapia</a>, 
+                                    <a href="capilar-carboxiterapia.php">Carboxiterapia Capilar</a>, 
+                                    <a href="capilar-led.php">Led Capilar</a> &nbsp<br> &nbsp<br>&nbsp<br>
+                                </p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-4 col-xs-12">
+                            <figure class="wow fadeInLeft animated portfolio-item" data-wow-duration="500ms" data-wow-delay="3ms">
+                                <div class="img-wrapper">
+                                    <img src="images/portfolio/pos_home.gif" class="img-responsive" alt="Pós Operatório" >
+                                    <div class="overlay">
+                                        <div class="buttons" style="left: 30%">
+                                            <a href="pos_operatorio.php">Mais Detalhes</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <figcaption>
+                                <h4><a href="pos_operatorio.php">Pós Operatório</a></h4>
+                                <p align="justify">
+                                    A eficiência de um procedimento cirúrgico não depende apenas da cirurgia em si. 
+                                        É necessário a atenção também nos cuidados  pré e pós-operatórios .&nbsp<br>&nbsp<br>
+                                </p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                       
+                        <div class="col-sm-4 col-xs-12">
+                            <figure class="wow fadeInLeft animated portfolio-item" data-wow-duration="500ms" data-wow-delay="3ms">
+                                <div class="img-wrapper">
+                                    <img src="images/portfolio/corporal/peim.jpg" class="img-responsive" alt="PEIM" >
+                                    <div class="overlay">
+                                        <div class="buttons" style="left: 30%">
+                                            <a href="corporal-peim.php">Mais Detalhes</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <figcaption>
+                                    <h4><a href="corporal-peim.php">PEIM</a></h4>
+                                <p align="justify">
+                                   O Procedimento estético injetável para microvasos (PEIM) é um tratamento realizado 
+                                   com o objetivo de eliminar os vasinhos que aparecem nas pernas e se tornam um incomodo estético para as mulheres. 
+                                </p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div class="col-sm-4 col-xs-12">
+                            <figure class="wow fadeInLeft animated portfolio-item" data-wow-duration="500ms" data-wow-delay="3ms">
+                                <div class="img-wrapper">
+                                    <img src="images/portfolio/nutricao/nutricao.jpg" class="img-responsive" alt="PEIM" >
+                                    <div class="overlay">
+                                        <div class="buttons" style="left: 30%">
+                                            <a href="nutricao.php">Mais Detalhes</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <figcaption>
+                                    <h4><a href="corporal-peim.php">Nutrição</a></h4>
+                                <p align="justify">
+                                   Na Haut você encontra pacotes que unem nutrição e estética, afim de melhorar os seus resultados
+                                   e facilitar o processo de perda de peso. Fizemos um trabalho personalisado, de acordo com suas necessidades e objetivos.
+                                </p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+            </section> 
+            <!--==================================================
+            Portfolio Section Start
+            ================================================== -->
+            <section id="feature">
+                <div class="container">
+                    <div class="col-md-12">
+                         <div class="map-area">
+                             <iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d870.9683968046974!2d-51.51311541190636!3d-29.16839719888262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x951c2352ee21b363%3A0x9a41e1c46234a684!2sCondom%C3%ADnio+Edif%C3%ADcio+Centro+Comercial+13+de+Maio+-+R.+13+De+Maio%2C+581+-+Centro%2C+Bento+Gon%C3%A7alves+-+RS%2C+95700-000!5e0!3m2!1spt-BR!2sbr!4v1509965819176" class="map" frameborder="0"  allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </section> <!-- /#feature -->
+            <!-- ===========================Contato footer================================================== -->
+            <?php include_once 'contato_footer.php';?>
+            <!-- ==================================Footer Section Start================================================== -->
+            <?php include_once 'footer.php';?>
+            <script type="text/javascript" src="js/banner/jquery.nivo.slider.js"></script>
+            <script type="text/javascript">
+            $(window).load(function() {
+                $('#slider').nivoSlider();
+            });
+            </script>
+        </body>
+    </html>
